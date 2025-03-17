dfc49d18163797ed1065e46230cb6ad2782eaa0e
Antonn-Retiers <a.damournicolas23@assomption.bzh>
2025-03-17 09:30:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿function updateRecipe() {
+    const servings = parseInt(document.getElementById('servings').value);
+    
+    document.getElementById('eggs').textContent = 4 * servings;
+    document.getElementById('milk').textContent = 50 * servings;
+    document.getElementById('cheese').textContent = 30 * servings;
+	document.getElementById('lardon').textContent = 150 * servings;
+}
+
+// Initial calculation on page load
+updateRecipe();
